86a88ea4c9a2ad4701e65b841b7c23ada6ebf342
NJ-JayJay <metalmedley1@gmail.com>
2019-05-14 14:09:09 +0100

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,36 @@
-class Student
+  class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    pupil = Student.new
+    pupil.id = row[0]
+    pupil.name = row[1]
+    pupil.grade = row[2]
+    pupil
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM students;"
+    data = DB[:conn].execute(sql)
+    arr = []
+    data.each do |row|
+      s = self.new_from_db(row)
+      arr << s
+    end
+    arr
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = '#{name}' LIMIT 1;"
+    #print sql
+    row = DB[:conn].execute(sql)
+    #print row
+    self.new_from_db(row[0])
   end
   
   def save
@@ -40,4 +58,45 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = "SELECT name FROM students WHERE grade = 9;"
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT name FROM students WHERE grade < 12;"
+    data = DB[:conn].execute(sql)
+    #print data[0][0]
+    [self.find_by_name(data[0][0])]
+  end   
+
+  def self.first_X_students_in_grade_10(param)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT #{param};"
+    data = DB[:conn].execute(sql)
+    puts data
+    arr = []
+    data.each do |row|
+      arr << self.find_by_name(row[1])
+    end
+    arr
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY id ASC LIMIT 1;"
+    data = DB[:conn].execute(sql)
+    self.find_by_name(data[0][1])
+  end
+
+  def self.all_students_in_grade_X(param)
+    sql = "SELECT * FROM students WHERE grade = #{param};"
+    data = DB[:conn].execute(sql)
+    arr = []
+    data.each do |row|
+      arr << self.find_by_name(row[1])
+    end
+    arr
+  end
+
+  
 end
---
+++ seed.sql
@@ -0,0 +1,102 @@
+CREATE TABLE owners(id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE pets(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, type TEXT, owner_id INTEGER);
+INSERT INTO owners(id, name) VALUES (0,'Serita Wolf');
+INSERT INTO owners(id, name) VALUES (1,'Cary Price');
+INSERT INTO owners(id, name) VALUES (2,'Raguel Haag');
+INSERT INTO owners(id, name) VALUES (3,'Christy Schamberger');
+INSERT INTO owners(id, name) VALUES (4,'Jennette Hettinger');
+INSERT INTO owners(id, name) VALUES (5,'Maurice Heidenreich');
+INSERT INTO owners(id, name) VALUES (6,'Reagan Roob');
+INSERT INTO owners(id, name) VALUES (7,'Reinaldo Casper');
+INSERT INTO owners(id, name) VALUES (8,'Susanne Wolff');
+INSERT INTO owners(id, name) VALUES (9,'Carlton Feil');
+INSERT INTO owners(id, name) VALUES (10,'Art Macejkovic');
+INSERT INTO owners(id, name) VALUES (11,'Aletha Beatty');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Bart', '3', 'fish', '0');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Ana', '3', 'hamster', '0');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Woodrow', '3', 'geko', '0');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Joshua', '2', 'dog', '0');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Sabrina', '3', 'fish', '0');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Rodger', '2', 'fish', '1');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Emerson', '1', 'dog', '2');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Pat', '2', 'fish', '2');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Noreen', '1', 'cat', '2');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Alvin', '1', 'cat', '2');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Della', '2', 'lama', '2');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Toby', '3', 'fish', '3');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Jonathon', '2', 'lama', '3');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Gennie', '2', 'lama', '3');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Dominga', '1', 'geko', '3');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Kindra', '1', 'cat', '4');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Milan', '2', 'cat', '4');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Georgie', '3', 'cat', '4');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Shizue', '1', 'cat', '4');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Booker', '2', 'lama', '4');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Rosita', '3', 'geko', '5');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Dexter', '1', 'dog', '5');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Hershel', '1', 'cat', '5');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Siu', '3', 'cat', '6');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Glen', '2', 'cat', '6');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Christal', '3', 'fish', '6');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Jeffry', '2', 'cat', '7');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Lilian', '1', 'fish', '7');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Mara', '1', 'fish', '8');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Chung', '1', 'cat', '8');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Jesusa', '1', 'cat', '9');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Shirl', '1', 'geko', '9');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Dong', '2', 'cat', '9');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Warren', '3', 'lama', '9');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Cary', '3', 'hamster', '9');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Milo', '3', 'hamster', '10');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Lynette', '1', 'lama', '10');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Deana', '3', 'fish', '10');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Brynn', '2', 'geko', '10');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Salvatore', '1', 'cat', '11');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Keva', '2', 'geko', '11');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Ladonna', '1', 'dog', '11');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('King', '2', 'dog', '11');
+        INSERT INTO pets(name, age, type, owner_id)
+        VALUES ('Nicolas', '2', 'hamster', '11');
