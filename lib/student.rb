249c74f190dd8311d5611767c26e75ba2dd7f6ce
richardcrng@gmail.com <richardcrng@gmail.com>
2019-08-13 11:45:24 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,10 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
-  end
-
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-  end
-
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+  def initialize(attributes = {})
+    attributes.each_pair do |key, val|
+      send("#{key}=", val)
+    end
   end
   
   def save
@@ -22,6 +14,24 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql).map &db_row_to_instances
+  end
+
+  def self.all_students_in_grade_X(n)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, n).map &db_row_to_instances
+  end
+
+  def self.all_students_in_grade_9
+    all_students_in_grade_X(9)
   end
   
   def self.create_table
@@ -39,5 +49,37 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql, name).first
+    new_from_db(row)
+  end
+
+  def self.first_student_in_grade_10
+    all_students_in_grade_X(10).first
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    all_students_in_grade_X(10).first(x)
+  end
+
+  def self.db_row_to_instances
+    lambda { |row| new_from_db(row) }
+  end
+
+  def self.new_from_db(row)
+    id, name, grade = row
+    new(id: id, name: name, grade: grade)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map &db_row_to_instances
   end
 end
